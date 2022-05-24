df12c3ed5065a4bc8f0ad456ac30502cce2e5600
Felix Kiptoo <felixkpt@gmail.com>
2022-05-24 17:35:55 +0300

    Updates on admin page create & post create -added slug to create/update form

--- config/siteinfo.php
+++ config/siteinfo.php
@@ -1,6 +1,6 @@
 <?php 
 return [
     'name' => 'LegalPremiums',
-    'title' => 'View Authentic Reviews and Ratings on Popular Insurance Companies',
-    'description' => 'View Authentic Reviews and Ratings on popular insurance companies',
+    'title' => 'Authentic Reviews and Ratings on Popular Insurance Companies',
+    'description' => 'View Authentic Reviews and Ratings on popular insurance companies in US and around the world.',
 ];
