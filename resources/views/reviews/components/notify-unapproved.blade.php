d1bac0016aad88c2173f2244189922cdb12c3916
Felix Kiptoo <felixkpt@gmail.com>
2022-05-28 22:49:06 +0300

    Notify unapproved review

--- resources/views/reviews/components/notify-unapproved.blade.php
+++ resources/views/reviews/components/notify-unapproved.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-wrap w-full">
         <div class="w-full">
             <div class="alert alert-info flex flex-wrap w-full justify-between">
-                <div>
+                <div class="my-auto">
                     <strong>Info: </strong>Your review is awaiting moderation. In the mean time you can edit where necessary.
                 </div>
                 <a class="btn btn-sm main-outline-btn pt-2" href="{{ url('add-a-review/'.$review->post_id) }}">Edit my review</a>
